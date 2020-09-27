2e17c72c86b48c94f2890812881b2f42fd3430a1
Dungngo4520 <dungngo4520@gmail.com>
2020-09-27 19:23:20 +0700

    tiktok server commit

---
+++ TikTok-backend/data.js
@@ -0,0 +1,13 @@
+const data = [
+    {
+        url: "https://thumbs.gfycat.com/PlayfulNextBarasingha-mobile.mp4",
+        channel: "dungngo4520",
+        description: "Tiktok app i cloned, yeaaa 🔥🔥🔥",
+        song: "hmmmmmmmmmmmmmmmm",
+        likes: "6969",
+        messages: "696",
+        shares: "69",
+    },
+];
+
+export default data;
--- TikTok-backend/server.js
+++ TikTok-backend/server.js
@@ -1,9 +1,55 @@
-import express from 'express'
-import mongoose from 'mongoose'
+import express from 'express';
+import mongoose from 'mongoose';
 
+import Data from './data.js';
+import Videos from './dbModel.js';
+
+// app config
 const app = express();
 const port = 9000;
 
+// middlewares
+app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeaders('Access-Control-Allow-Origin', '*'),
+        res.setHeaders('Access-Control-Allow-Headers', '*'),
+        next();
+})
+
+// DB config
+mongoose.connect("mongodb+srv://admin:CzmcH8iyoCFH04g9@cluster0.od9dr.mongodb.net/tiktok-clone?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+
+// api endpoints
 app.get('/', (req, res) => res.status(200).send('Hello World'));
 
+app.get('/v1/posts', (req, res) => res.status(200).send(Data));
+
+app.get('/v2/posts', (req, res) => {
+    Videos.find((err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    });
+});
+
+app.post('/v2/posts', (req, res) => {
+    // add DATA to the database 
+    const dbVideos = req.body;
+
+    Videos.create(dbVideos, (err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(201).send(data);
+        }
+    });
+});
+
+// listener
 app.listen(port, () => console.log(`Listening on localhost: ${port}`));
